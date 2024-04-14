2a722a8ab11de2df75d786a30d1e397423ff2b5c
like-a-hacker <goethamramesh199@gmail.com>
2024-04-14 22:21:46 +0530

    new changes are there

--- public/index.js
+++ public/index.js
@@ -8,14 +8,14 @@
 
     document.querySelector("#submit").addEventListener("click",function(){
         let joinid = generateID();
-        document.querySelector("#join-id").innerHTML = `<span>${joinid}</span>`;
+        document.querySelector(".room").innerHTML = `<span>${joinid}</span>`;
         socket.emit("sender-join", {
             uid:joinid
         });
     });
     socket.on("init", function(uid){
         recid = uid;
-        document.querySelector(".up active").classList.remove("active");
-        document.querySelector(".form-input").classList.add("active");
+        document.querySelector(".room active").classList.remove("active");
+        document.querySelector(".up").classList.add("active");
     })
 })
