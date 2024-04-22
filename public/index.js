c18b2f9e0b198ca1d018568dbefb397a7c5e8d00
like-a-hacker <goethamramesh199@gmail.com>
2024-04-22 10:39:00 +0530

    pro:some changes

--- public/index.js
+++ public/index.js
@@ -3,19 +3,19 @@
     const socket = io();
 
     function generateID(){
-        return `${Math.trunc(Math.random()*999)}-${Math.trunc(Math.random()*999)}`;
+        return `${Math.trunc(Math.random()*999999)}`;
     }
 
     document.querySelector("#submit").addEventListener("click",function(){
         let joinid = generateID();
-        document.querySelector(".room").innerHTML = `<span>${joinid}</span>`;
+        document.querySelector(".room").innerHTML= `<span>${joinid}</span>`;
         socket.emit("sender-join", {
             uid:joinid
         });
     });
     socket.on("init", function(uid){
         recid = uid;
-        document.querySelector(".room active").classList.remove("active");
+        document.querySelector(".room").classList.remove("active");
         document.querySelector(".up").classList.add("active");
     })
 })
