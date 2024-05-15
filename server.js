5eb0275e5ca94b757d7b2422f89019c73930981b
like-a-hacker <goethamramesh199@gmail.com>
2024-05-15 07:55:59 +0530

    Proj:updated

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 const express = require("express");
 const path = require("path");
 
@@ -9,23 +8,23 @@ const io = require("socket.io")(server);
 
 app.use(express.static(path.join(__dirname+"/public")));
 
-io.on("connection",function(socket){
-    socket.on("sender-join",function(data){
-        socket.join(data.uid);
-    });
-    socket.on("recever-join",function(data){
-        socket.join(data.uid);
-        socket.in(data.uid).emit("init",data.uid);
-    });
-    socket.on("file-meta",function(data){
-        socket.in(data.uid).emit("fs-meta",data.metedata);
-    });
-    socket.on("fs-start",function(data){
-        socket.in(data.uid).emit("fs-share",{});
-    });
-    socket.on("fs-raw",function(data){
-        socket.in(data.uid).emit("fs-share",data.buffer);
-    });
-})
+io.on("connection", function(socket){
+	socket.on("sender-join",function(data){
+		socket.join(data.uid);
+	});
+	socket.on("receiver-join",function(data){
+		socket.join(data.uid);
+		socket.in(data.sender_uid).emit("init", data.uid);
+	});
+	socket.on("file-meta",function(data){
+		socket.in(data.uid).emit("fs-meta", data.metadata);
+	});
+	socket.on("fs-start",function(data){
+		socket.in(data.uid).emit("fs-share", {});
+	});
+	socket.on("file-raw",function(data){
+		socket.in(data.uid).emit("fs-share", data.buffer);
+	})
+});
 
 server.listen(5000);
